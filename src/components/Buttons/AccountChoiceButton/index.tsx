7d4975221ee2c203a3634817cd8abdc96262838e
Yeferson <yefersonsmarter@gmail.com>
2020-11-26 17:01:33 -0430

    fix css problem in AccountChoiceButtonComponent

--- src/components/Buttons/AccountChoiceButton/index.tsx
+++ src/components/Buttons/AccountChoiceButton/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
 import { IAccountChoiceButtonProps } from './AccountChoiceButtonPropsInterface'
+import './style.css'
 
 
 export const AccountChoiceButton = (props: IAccountChoiceButtonProps) : JSX.Element => {
