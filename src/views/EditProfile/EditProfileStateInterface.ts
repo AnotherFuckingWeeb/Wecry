f71ab3dc2ea2e742333f4b313be8bf327a492339
Yeferson <yefersonsmarter@gmail.com>
2020-12-22 23:29:20 -0430

    added state

---
+++ src/views/EditProfile/EditProfileStateInterface.ts
@@ -0,0 +1,8 @@
+export interface IEditProfileState {
+    image: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    currentPassword: string;
+    newPassword: string;
+}
--- src/views/EditProfile/index.tsx
+++ src/views/EditProfile/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { IEditProfileProps } from './EditProfilePropsInterface'
+import { IEditProfileState } from './EditProfileStateInterface'
 import { SearchBar } from '../../components/Navbars/SearchBar'
 import { InputText } from '../../components/InputText'
 import { Header } from '../../components/Header'
@@ -8,7 +10,30 @@ import { EditProfileInfo } from '../../components/EditProfile/EditProfileInfo'
 import { EditProfileInputSection } from '../../components/EditProfile/EditProfileInputSection'
 import './style.css'
 
-class EditProfile extends React.Component {
+class EditProfile extends React.Component<IEditProfileProps, IEditProfileState> {
+    
+    constructor(props: IEditProfileProps) {
+        super(props)
+
+        this.state = {
+            image: 'http://localhost:3000/static/media/1604457981001.b21f8af2.jpg',
+            firstname: '',
+            lastname: '',
+            email: '',
+            currentPassword: '',
+            newPassword: ''
+        }
+    }
+
+    onChangeState = (event: React.ChangeEvent<HTMLInputElement>) : void => {
+        const { name, value } = event.target
+    
+        this.setState({
+            ...this.state,
+            [name]: value
+        })
+    }
+    
     render() :  JSX.Element {
         return (
             <main>
@@ -22,20 +47,20 @@ class EditProfile extends React.Component {
                         <div style={{marginBottom: 20}} >
                             <Header title='Edit Profile' fontSize={30} />
                         </div>
-                        <EditProfileInfo image='http://localhost:3000/static/media/1604457981001.b21f8af2.jpg' fullname='Yeferson Hidalgo' email='yefersonsmarter@gmail.com' />
+                        <EditProfileInfo image={this.state.image} fullname='Yeferson Hidalgo' email='yefersonsmarter@gmail.com' />
                         
                         <EditProfileInputSection width='80%' height={150} justifyContent='space-between' >
-                            <InputText placeholder='Firstname' name='' value='' type='text' onChange={() => {}} />
-                            <InputText placeholder='Lastname' name='' value='' type='text' onChange={() => {}} />
+                            <InputText placeholder='Firstname' name='firstname' value={this.state.firstname} type='text' onChange={this.onChangeState} />
+                            <InputText placeholder='Lastname' name='lastname' value={this.state.lastname} type='text' onChange={this.onChangeState} />
                         </EditProfileInputSection>
                         
                         <EditProfileInputSection width='80%' height={100} justifyContent='center' >
-                            <InputText placeholder='Email' name='' value='' type='email' onChange={() => {}} />
+                            <InputText placeholder='Email' name='email' value={this.state.email} type='email' onChange={this.onChangeState} />
                         </EditProfileInputSection>
 
                         <EditProfileInputSection width='80%' height={150} justifyContent='space-between' >
-                            <InputText placeholder='Current Password' name='' value='' type='text' onChange={() => {}} />
-                            <InputText placeholder='New Password' name='' value='' type='text' onChange={() => {}} />
+                            <InputText placeholder='Current Password' name='currentPassword' value={this.state.currentPassword} type='text' onChange={this.onChangeState} />
+                            <InputText placeholder='New Password' name='newPassword' value={this.state.newPassword} type='text' onChange={this.onChangeState} />
                         </EditProfileInputSection>
                         
                         <EditProfileInputSection width='80%' height={100} justifyContent='center' >
